e42d35d0d9d3980e92c5f9cc0e8e45a22ccbe17e
Devin Miller <email": null>
2017-10-27 14:50:14 -0700

    tests for user create with valid input

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,4 +13,23 @@ RSpec.describe UsersController, type: :controller do
       expect(assigns[:user]).to be_a User
     end
   end
-end
+  describe "create" do
+    context "when user enters valid input " do
+      before(:each) do
+        post :create, params: {user: {username: "Dev", email: "devbootcamp@camp.com", password: 'ham', password_confirmation: 'ham'}}
+      end
+      it "assigns a user variable" do
+        expect(assigns[:user]).to be_a User
+      end
+      it "creates a user in the user table" do
+        expect(User.find_by(email: "devbootcamp@camp.com")).to be_a User
+      end
+      it "sets the current user session" do
+        expect(session[:user_id]).to_not be nil
+      end
+      it "redirects to new user path " do
+        expect(response).to redirect_to new_user_path
+      end
+    end
+  end
+end#end of UsersController
