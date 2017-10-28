c2501280794ed08d1b412db8b29d10aba3b59bd8
Devin Miller <email": null>
2017-10-28 11:54:51 -0700

    Style tag checkboxes to look like buttons

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -3,9 +3,12 @@ class RecommendationsController < ApplicationController
   end
 
   def new
-    @doctor = Doctor.create(first_name: "Lucy")
+    @doctor = Doctor.create(first_name: "Lucy", last_name: "Niflheim")
     @recommendation = Recommendation.new(doctor: @doctor, user: current_user)
     render :new
+  end
+
+  def create
   end
 
 end
