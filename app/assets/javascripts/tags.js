f168c98b39b43eee7748be51e9ff67f396c139a2
Devin Miller <email": null>
2017-10-28 13:53:06 -0700

    Add jquery to add a checked tag on new tag submission

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -1,3 +1,12 @@
 $(document).ready(() => {
-  
+  $(".add-tag").on("click", (event) => {
+    event.preventDefault();
+    const tagDesc = $("#recommendation_tags_description").val();
+    const tagUnderscores = tagDesc.replace(" ", "_")
+    let tagHTML = `<li>
+        <input class="tag" type="checkbox" value="${tagDesc}" name="recommendation[tags][]" id="recommendation_tags_${tagDesc}" checked>
+        <label class="tag-label" for="recommendation_tags_${tagUnderscores}">${tagUnderscores}</label>
+      </li>`
+    $(".tag-list").append(`${tagHTML}`);
+  })
 });
