0644f7b35afdbd67a57bad3282ea650cbe379751
Devin Miller <email": null>
2017-10-27 19:06:51 -0700

    Write passing associations tests for recommendation model

--- spec/models/recommendation_spec.rb
+++ spec/models/recommendation_spec.rb
@@ -2,12 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Recommendation, type: :model do
   describe 'associations' do
-
-    it 'belongs to a doctor' do
-      expect(rec.doctor).to eq doctor
-    end
-    it 'belongs to a user' do
-      expect(rec.user).to eq user
-    end
+    it {should belong_to(:doctor)}
+    it {should belong_to(:user)}
   end
 end
