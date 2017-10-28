83f96ea6c370b2cc2f99d7c89312019f8fd42d4e
Devin Miller <email": null>
2017-10-27 19:04:40 -0700

    Create doctor and recommendation factories

---
+++ spec/factories/doctor_factory.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :doctor do
+    first_name "Georgette"
+    last_name "Tronkenheim"
+    specialty "Family Practice"
+    gender "female"
+    street "33 Orange St"
+    city "Seattle"
+    state "WA"
+    zipcode 98103
+    phone_number 2065559999
+    website "cats.com"
+    email_address "georgette@doctor.com"
+  end
+end
--- spec/models/recommendation_spec.rb
+++ spec/models/recommendation_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Recommendation, type: :model do
   describe 'associations' do
-    it 'belongs to a doctor'
-    it 'belongs to a user'
+
+    it 'belongs to a doctor' do
+      expect(rec.doctor).to eq doctor
+    end
+    it 'belongs to a user' do
+      expect(rec.user).to eq user
+    end
   end
 end
