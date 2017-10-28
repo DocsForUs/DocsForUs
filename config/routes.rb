0fc75ec7683f28e54d8dfed406e464e01562eb5e
Devin Miller <email": null>
2017-10-28 13:16:13 -0700

    Add new tag field on new recommendation form

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :doctors, only: [:index]
   resources :recommendations, only: [:new, :create]
+  resources :tags, only: :create
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
