ddc125159c39dfe90960bb8b531c1629bac6328b
Amanuel <yihdego@msn.com>
2017-10-28 12:08:59 -0700

    methods for api call and controller added tests to do

---
+++ app/controllers/better_doctors_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::DoctorsController < ApplicationController
+  caches_action :search, cache_path: Proc.new { |c| c.request.url }, expires_in: 30.minutes
+
+  def search
+    render json: doctors_service.doctors(search_params, filter_params)
+  end
+
+protected
+  def search_params
+    params.permit(:name)
+  end
+
+  def filter_params
+    params.permit(:skip, :limit)
+  end
+
+  def doctors_service
+    BetterDoctorService.new({
+      user_key: ENV["BETTER_DOCTOR_USER_KEY"]
+    })
+  end
+end
