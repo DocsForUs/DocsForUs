967773f86e403da36bd77b2d6fe0d8214c7085d3
Devin Miller <email": null>
2017-10-27 19:09:32 -0700

    Write passing tests for recommendation model validations

--- spec/models/recommendation_spec.rb
+++ spec/models/recommendation_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Recommendation, type: :model do
+  let!(:rec) { create(:recommendation) }
   describe 'associations' do
     it {should belong_to(:doctor)}
     it {should belong_to(:user)}
+    it 'is invalid without a doctor' do
+      rec.doctor = nil
+      expect(rec).to_not be_valid
+    end
+    it 'is invalid without a user' do
+      rec.user = nil
+      expect(rec).to_not be_valid
+    end
   end
 end
