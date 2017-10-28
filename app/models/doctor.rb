a1c09fa6b644bb413785c1734efcf118895c88fa
Amanuel <yihdego@msn.com>
2017-10-28 15:38:37 -0700

    removed limit 2 in api call

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,11 +2,8 @@ class Doctor < ApplicationRecord
   validates :first_name, :last_name, :specialty, :zipcode, presence: true
   validate :email_xor_phone_number
 
-
-  def self.search_api(doctor)
-    full_name = doctor[:first_name] +' '+ doctor[:last_name]
-    location = doctor[:state] + '-' + doctor[:city]
-    HTTParty.get("https://api.betterdoctor.com/2016-03-01/doctors?name=#{full_name}&location=#{location}&skip=2&limit=10&user_key=#{ENV['BETTER_DOCTOR_USER_KEY']}")
+  def self.search_doctor(doctor)
+    Doctor.search_api(doctor)
   end
 
   private
@@ -14,5 +11,12 @@ class Doctor < ApplicationRecord
     if email_address.blank? && phone_number.blank?
       errors.add(:base, "Specify either phone number or email address of the Doctor")
     end
+  end
+
+  def self.search_api(doctor)
+    full_name = doctor[:first_name] +' '+ doctor[:last_name]
+    location = doctor[:state] + '-' + doctor[:city]
+    p 'this is api call'
+    p HTTParty.get("https://api.betterdoctor.com/2016-03-01/doctors?name=#{full_name}&location=#{location}&limit=10&user_key=#{ENV['BETTER_DOCTOR_USER_KEY']}", format: :plain)
   end
 end
--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,12 +1,30 @@
 require 'rails_helper'
+require 'json'
 
 describe Doctor, type: :model do
   context 'searches the api with first and last name parameters' do
-    it 'makes a reponse body that is not nil' do
+    xit 'returns a single doctor with narrow enough results' do
       doctor = {first_name: "John", last_name: "Anderson", city: "seattle", state: "wa"}
       response = Doctor.search_api(doctor)
-      p response.body
       expect(response["meta"]["total"]).to eq 1
+    end
+
+    xit 'returns mutliple doctors in a wide result' do
+      doctor = {first_name: "John", last_name: "", city: "seattle", state: "wa"}
+      response = Doctor.search_api(doctor)
+
+      expect(response["meta"]["total"]).to be > 1
+    end
+
+    it 'returns hash with all of the important information of a doctor' do
+      doctor = {first_name: "Sara", last_name: "Waterman", city: "seattle", state: "wa"}
+      response = Doctor.search_doctor(doctor)
+      p 'this is before parse'
+      p response.body
+      response =  JSON.parse response.body, symbolize_names: true
+      p 'this is after parse'
+      p response
+      expect(response).to eq nil
     end
   end
 end
