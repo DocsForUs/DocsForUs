9de8189ec3b9ec06e1ec533c533b9807590128d8
Amanuel <yihdego@msn.com>
2017-10-27 19:09:46 -0700

    add another test for assigns doctors

--- spec/controllers/doctors_controller_spec.rb
+++ spec/controllers/doctors_controller_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 RSpec.describe DoctorsController, type: :controller do
   let(:doctor) { Doctor.create(first_name: 'Elizabeth', last_name: 'Blackwell') }
+  let(:doctor2) { Doctor.create(first_name: 'Atul', last_name: 'Gawande') }
   describe 'index route for searching' do
     it 'assigns a instance @doctors to doctors that fit the search result' do
       get :index, params: { doctor: {first_name: 'Elizabeth', last_name: 'Blackwell'} }
       expect(assigns[:doctors]).to include(doctor)
     end
+    it "it does not return doctors that do not fit the search request" do
+      get :index, params: { doctor: {first_name: 'Elizabeth', last_name: 'Blackwell'} }
+      expect(assigns[:doctors]).to_not include(doctor2)
+    end
+
   end
 
 end
