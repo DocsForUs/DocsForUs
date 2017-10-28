8b0ca0eecaa8eef985c98241a5fe0b6d03ceb085
Devin Miller <email": null>
2017-10-28 15:43:26 -0700

    Add container and some classes to new recommendation form for styling purposes. Add some styling.

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -3,7 +3,7 @@ $(document).ready(() => {
     event.preventDefault();
     const tagDesc = $("#new_tag_description").val();
     const tagUnderscores = tagDesc.replace(" ", "_")
-    let tagHTML = `<li>
+    let tagHTML = `<li class="tag-li">
         <input class="tag" type="checkbox" value="${tagDesc}" name="recommendation[tags][]" id="recommendation_tags_${tagDesc}" checked>
         <label class="tag-label" for="recommendation_tags_${tagUnderscores}">${tagUnderscores}</label>
       </li>`
