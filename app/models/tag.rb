8afe0ec382b1b81b7252a111cfe53e3a4fbd9f4b
Devin Miller <email": null>
2017-10-27 19:35:24 -0700

    Add tag validations to model and write passing tests

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ApplicationRecord
   has_many :recommendations_tags
   has_many :recommendations, through: :recommendations_tags
+  validates :description, presence: true, uniqueness: true
 end
