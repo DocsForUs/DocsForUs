484307a29ed9c6c9d3f32e5a4d02cef45207537e
Ashishjayandar & Amanuel <pair+ashishjayandar.yihdego@devbootcamp.com>
2017-10-28 19:39:20 -0700

    redid migration for interger, the recommend button takes named arguments of api results

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -3,37 +3,7 @@ require 'json'
 
 describe Doctor, type: :model do
   context 'searches the api with first and last name parameters' do
-    xit 'returns a single doctor with narrow enough results' do
-      doctor = {first_name: "John", last_name: "Anderson", city: "seattle", state: "wa"}
-      response = Doctor.search_api(doctor)
-      expect(response["meta"]["total"]).to eq 1
-    end
-
-    xit 'returns mutliple doctors in a wide result' do
-      doctor = {first_name: "John", last_name: "", city: "seattle", state: "wa"}
-      response = Doctor.search_api(doctor)
-      expect(response["meta"]["total"]).to be > 1
-    end
-
-    xit 'returns hash with all information of a single doctor' do
-      doctor = {first_name: "Sara", last_name: "Waterman", city: "seattle", state: "wa"}
-      response = Doctor.search_api(doctor)
-      response =  JSON.parse response.body, symbolize_names: true
-      expect(response[:meta][:total]).to eq 1
-    end
-
-    xit 'returns the first name of a doctor from an api call' do
-      doctor = {first_name: "Sara", last_name: "Waterman", city: "seattle", state: "wa"}
-      response = Doctor.search_doctor(doctor)
-      expect(response).to eq 'Sara'
-    end
-
-    xit 'returns the city a doctor from an api call' do
-      doctor = {first_name: "Sara", last_name: "Waterman", city: "seattle", state: "wa"}
-      response = Doctor.search_doctor(doctor)
-      expect(response).to eq 'Seattle'
-    end
-
+    
     xit 'returns an array of a single doctors information parsed' do
       doctor = {first_name: "Sara", last_name: "Waterman", city: "seattle", state: "wa"}
       response = Doctor.search_doctor(doctor)
