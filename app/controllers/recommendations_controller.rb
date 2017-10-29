33df6bd8b22a2f23a7eb073a9052fbbb3f1ebff6
Devin Miller <email": null>
2017-10-28 19:24:47 -0700

    Restrict recommendations#new and #create to logged in users

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -3,24 +3,32 @@ class RecommendationsController < ApplicationController
   end
 
   def new
-    @doctor = Doctor.create(first_name: "Lucy", last_name: "Niflheim")
-    @recommendation = Recommendation.new(doctor: @doctor, user: current_user)
-    @tags = Tag.default_tags
-    @tag = Tag.new
-    render :new
+    if current_user
+      @doctor = Doctor.create(first_name: "Lucy", last_name: "Niflheim")
+      @recommendation = Recommendation.new(doctor: @doctor, user: current_user)
+      @tags = Tag.default_tags
+      @tag = Tag.new
+      render :new
+    else
+      redirect_to root_path
+    end
   end
 
   def create
-    @recommendation = Recommendation.new(rec_params)
-    @recommendation.user = current_user
-    tags = params[:recommendation][:tags]
-    if tags == nil
-      @errors = "You must choose at least one tag."
-      render :new
+    if current_user
+      @recommendation = Recommendation.new(rec_params)
+      @recommendation.user = current_user
+      tags = params[:recommendation][:tags]
+      if tags == nil
+        @errors = "You must choose at least one tag."
+        render :new
+      else
+        tags.map! { |tag| Tag.find_or_create_by(description: tag)}
+        @recommendation.tags << tags
+        @recommendation.save
+        redirect_to root_path
+      end
     else
-      tags.map! { |tag| Tag.find_or_create_by(description: tag)}
-      @recommendation.tags << tags
-      @recommendation.save
       redirect_to root_path
     end
   end
--- spec/controllers/recommendations_controller_spec.rb
+++ spec/controllers/recommendations_controller_spec.rb
@@ -10,50 +10,66 @@ RSpec.describe RecommendationsController, type: :controller do
   end
 
   describe '#new' do
-    before(:each) do
-      get :new, session: {user_id: user.id}
+    context 'when user is logged in' do
+      before(:each) do
+        get :new, session: {user_id: user.id}
+      end
+      it 'assigns a doctor instance variable' do
+        expect(assigns[:doctor]).to be_a Doctor
+      end
+      it 'assigns a recommendation instance variable' do
+        expect(assigns[:recommendation]).to be_a Recommendation
+      end
+      it 'returns OK status' do
+        expect(response).to be_ok
+      end
+      it 'renders the new view' do
+        expect(response).to render_template :new
+      end
     end
-    it 'assigns a doctor instance variable' do
-      expect(assigns[:doctor]).to be_a Doctor
-    end
-    it 'assigns a recommendation instance variable' do
-      expect(assigns[:recommendation]).to be_a Recommendation
-    end
-    it 'returns OK status' do
-      expect(response).to be_ok
-    end
-    it 'renders the new view' do
-      expect(response).to render_template :new
+    context 'when user is not logged in' do
+      it 'redirects to root path' do
+        get :new
+        expect(response).to redirect_to root_path
+      end
     end
   end
 
   describe '#create' do
-    context 'when recommendation is created successfully' do
-      before(:each) do
-        post :create, params: {"recommendation"=>{"doctor_id"=>"116", "review"=>"fish fish fish", "tags"=>["ham"]}}, session: {user_id: user.id}
+    context 'when user is logged in' do
+      context 'when recommendation is created successfully' do
+        before(:each) do
+          post :create, params: {"recommendation"=>{"doctor_id"=>"116", "review"=>"fish fish fish", "tags"=>["ham"]}}, session: {user_id: user.id}
+        end
+        it 'creates a recommendation instance variable' do
+          expect(assigns[:recommendation]).to be_a Recommendation
+        end
+        it 'adds tags to the database if they do not already exist' do
+          expect { post :create, params: {"recommendation"=>{"doctor_id"=>"116", "review"=>"fish fish fish", "tags"=>["cats"]}}, session: {user_id: user.id} }.to change{Tag.count}.by(1)
+        end
+        it 'creates relationship between tags and the recommendation' do
+          expect(assigns[:recommendation].tags).to include(Tag.find_by(description: "ham"))
+        end
+        it 'redirects to root path' do
+          expect(response).to redirect_to root_path
+        end
       end
-      it 'creates a recommendation instance variable' do
-        expect(assigns[:recommendation]).to be_a Recommendation
-      end
-      it 'adds tags to the database if they do not already exist' do
-        expect { post :create, params: {"recommendation"=>{"doctor_id"=>"116", "review"=>"fish fish fish", "tags"=>["cats"]}}, session: {user_id: user.id} }.to change{Tag.count}.by(1)
-      end
-      it 'creates relationship between tags and the recommendation' do
-        expect(assigns[:recommendation].tags).to include(Tag.find_by(description: "ham"))
-      end
-      it 'redirects to root path' do
-        expect(response).to redirect_to root_path
+      context 'when recommendation is not created successfully' do
+        before(:each) do
+          post :create, params: {"recommendation"=>{"doctor_id"=>"116", "review"=>"fish fish fish", "tags"=>[]}}, session: {user_id: user.id}
+        end
+        it 'assigns an errors instance variable' do
+          expect(assigns[:errors]).to eq "You must choose at least one tag."
+        end
+        it 'renders the new recommendation view' do
+          expect(response).to render_template :new
+        end
       end
     end
-    context 'when recommendation is not created successfully' do
-      before(:each) do
-        post :create, params: {"recommendation"=>{"doctor_id"=>"116", "review"=>"fish fish fish", "tags"=>[]}}, session: {user_id: user.id}
-      end
-      it 'assigns an errors instance variable' do
-        expect(assigns[:errors]).to eq "You must choose at least one tag."
-      end
-      it 'renders the new recommendation view' do
-        expect(response).to render_template :new
+    context 'when user is not logged in' do
+      it 'redirects to root path' do
+        post :create, params: {"recommendation"=>{"doctor_id"=>"116", "review"=>"fish fish fish", "tags"=>["ham"]}}
+        expect(response).to redirect_to root_path
       end
     end
   end
