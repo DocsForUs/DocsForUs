7d558b161cc148dc293b353c8da9a2ddbff8f658
Eliza Brine <elizabrine@gmail.com>
2017-10-28 17:55:58 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :doctors, only: [:new, :create, :index, :show]
   resources :recommendations, only: [:new, :create]
-  resources :tags, only: :create
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
