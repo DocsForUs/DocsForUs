4e4405208d09ebbb58bd364c4ffd47ad21c8d992
Amanuel <yihdego@msn.com>
2017-10-31 13:37:10 -0700

    changed html routes for create and destroy doctors_users

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # resources :sessions, only: [:new, :create, :destroy]
   root to: "index#home"
-  resources :users, only: [:new, :create, :show] do
-    resources :doctors, only: [:create, :destroy]
-  end
+  resources :users, only: [:new, :create, :show]
   resources :doctors, only: [:new, :create, :index, :show] do
     collection do
       get 'find'
     end
   end
 
-  resources :doctor_users, only: [:create, :destroy]
   resources :recommendations, only: [:new, :create]
 
+  get '/save' => 'doctor_users#create'
+  get '/remove' => 'doctor_users#destroy'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
