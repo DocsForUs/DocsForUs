5f3af6993e5e810c82510b51ab12a09cb7a1d68d
Devin Miller & Piper Denney <pair+devzmiller.piperdenney@devbootcamp.com>
2017-10-31 16:34:25 -0700

    Add category tag seeds. Update tag model. Modified tags helper.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,14 +1,9 @@
-DEFAULT_TAGS = ["used correct pronouns", "was familiar with my medical needs", "said some stuff and things", "liked cats", "sandwiches"]
-
 class Tag < ApplicationRecord
   has_many :recommendations_tags
   has_many :recommendations, through: :recommendations_tags
   validates :description, :category, :default,  presence: true, uniqueness: true
 
   def self.default_tags
-    default_tags = []
-    DEFAULT_TAGS.each do |tag|
-      default_tags << Tag.find_by(description: tag)
-    end
+    Tag.where(default: true)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,13 @@ insurances.length.times do |i|
   ins = Insurance.create(insurance_uid: insurances[i][1], insurance_name: insurances[i][0])
   Doctor.all.sample.insurances << ins
 end
+
+Tag.destroy_all
+
+safe = ["transgender", "non-binary", "queer", "genderqueer", "trans masculine", "trans feminine", "polyamorous", "asexual", "gay", "lesbian", "bisexual", "pansexual", "latinx", "american indian", "asian", "black", "african american", "middle eastern", "multiracial", "intersex", "a person of color", "female", "male", "two-spirit"]
+
+competencies = ["sex workers", "youth", "teen", "adult", "blind or visually impaired", "deaf or hard hearing", "hiv positive",  "other abled", "cognitive disability", "developmental disability","mental health", "in recovery", "survivors of abuse or trauma", "undocumented", "uninsured", "experiencing addiction", "experiencing homelessness",  "veterans"]
+
+actions = ["asked for pronouns", "used my preferred name", "used my preferred pronouns", "was body positive", "was sex positive", "was fat positive", "respected my partner(s)", "was familiar with my medical concerns", "listened actively", "did not make harmful assumptions", "was up-to-date with current research", "avoided perpetuating stereotypes"]
+
+services = ["hormone therapy", "fertility", "top surgery", "lower surgery", "ffs", "fms", "clitoroplasty", "body sculpting", "breast augmentation", "buttock augmentation", "glansplasty", "hair grafting", "hysterectomy", "labiaplasty", "metoidioplasty", "orchiectomy", "penile implant", "phalloplasty", "scrotoplasty", "tracheal shave", "urethroplasty", "vaginectomy", "vaginoplasty", "reconstructive urology", "cognitive behavioral therapy", "neuro feedback", "acupuncture", "ivf", "endocrinology", "testesterone therapy", "speech therapy", "mental health", "counseling","transition counseling"]
