c1ad2e291235b84daf4c38c2dccf8b5c047be3a4
Devin Miller <email": null>
2017-10-31 08:54:47 -0700

    Rewrite test of .search_doctor to account for stubbed api response

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -6,17 +6,11 @@ describe Doctor, type: :model do
     it {should have_many(:users).through(:doctors_users) }
   end
   context 'searches the api with first and last name parameters' do
-    
-    it 'returns an array of a single doctors information parsed' do
-      doctor = {first_name: "Sara", last_name: "Waterman", city: "seattle", state: "wa"}
-      response = Doctor.search_doctor(doctor)
-      expect(response).to eq({:location=>[{:city=>"Seattle", :state=>"WA", :street=>"1200 12th Ave S", :zip=>"98144"}, {:city=>"Seattle", :state=>"WA", :street=>"1200 12th Ave S", :zip=>"98144"}], :first=>"Sara", :last=>"Waterman", :gender=>"female"})
-    end
 
     it 'returns an array of doctors information parsed' do
-      doctor = {first_name: "John", last_name: "Christopher", city: "seattle", state: "wa"}
+      doctor = {first_name: "Laura", last_name: "Spring", city: "Seattle", state: "WA"}
       response = Doctor.search_doctor(doctor)
-      expect(response.count).to eq 3
+      expect(response[0][:first_name]).to eq "Laura"
     end
 
     it "it returns an array of the doctor's insurances" do
