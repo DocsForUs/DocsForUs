b6f217a0f21dc43b0fad1dc328b14f738ed8d8a9
Amanuel <yihdego@msn.com>
2017-11-01 12:18:20 -0700

    users created as admin have attribute true

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let!(:user) {create(:user)}
-  let!(:admin) {User.create(username: 'admin', email: 'admin@email.com', password: 'P@ssword1')}
+  let!(:admin) {User.create(username: 'admin', email: 'admin@email.com', password: 'P@ssword1', admin: true)}
 
   describe 'admin attribute' do
-    it 'does not have admin privilege is attribute is false' do
+    it 'a normal user created has admin attribute as false' do
       expect(user.admin).to be false
+    end
+    it 'a user created with admin true will be an admin' do
+      expect(admin.admin).to be true
     end
   end
 end
