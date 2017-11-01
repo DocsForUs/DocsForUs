600bf9ee296e8130a672cde7589ff5956bc1818f
Ashishjayandar <email": null>
2017-10-31 17:15:49 -0700

    the resources page basic content laid

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
   get '/about' => 'index#about'
+  get '/resources' => 'index#about'
 
   get '/recommendations/add', to: "recommendations#add"
 end
