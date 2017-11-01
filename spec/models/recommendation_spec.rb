21f92aec6b9b8c87661e97ed00e3c8eee7a02657
Amanuel <yihdego@msn.com>
2017-11-01 13:11:32 -0700

    tested validation of only admins can remove posts

--- spec/models/recommendation_spec.rb
+++ spec/models/recommendation_spec.rb
@@ -2,17 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Recommendation, type: :model do
   let!(:rec) { create(:recommendation) }
+  # Cant use user factory with same test as recommendation factory - who knew
+  let!(:someuser) {User.create(username: 'bob', email: 'bob@email.com', password: 'P@ssword1', admin: false)}
+  let!(:admin) {User.create(username: 'admin', email: 'admin@email.com', password: 'P@ssword1', admin: true)}
   describe 'associations' do
     it {should belong_to(:doctor)}
     it {should belong_to(:user)}
     it {should have_many(:tags).through(:recommendations_tags)}
-    it 'is invalid without a doctor' do
-      rec.doctor = nil
-      expect(rec).to_not be_valid
+    describe 'validations' do
+      it 'is invalid without a doctor' do
+        rec.doctor = nil
+        expect(rec).to_not be_valid
+      end
+      it 'is invalid without a user' do
+        rec.user = nil
+        expect(rec).to_not be_valid
+      end
     end
-    it 'is invalid without a user' do
-      rec.user = nil
-      expect(rec).to_not be_valid
+
+    describe 'methods' do
+      it 'can be edited by users'
+      it 'can be deleted by admins' do
+        expect{ rec.remove(admin.id) }.to change{ Recommendation.count }.by -1
+      end
+      it 'cant be deleted by regular users' do
+        expect{ rec.remove(someuser.id) }.to change{ Recommendation.count }.by 0
+      end
     end
   end
 end
