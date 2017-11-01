82eb2e46ef7fd92645fc77afd2316cfd3d727678
Amanuel <yihdego@msn.com>
2017-11-01 14:38:15 -0700

    changed migration to add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030034859) do
+ActiveRecord::Schema.define(version: 20171101213452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,10 +73,10 @@ ActiveRecord::Schema.define(version: 20171030034859) do
     t.string "username", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
-    t.boolean "admin", default: false
-    t.boolean "superadmin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
+    t.boolean "superadmin", default: false
   end
 
 end
