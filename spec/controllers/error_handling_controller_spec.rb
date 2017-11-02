8965227ade98890547016c138647383707887a2c
Amanuel <yihdego@msn.com>
2017-11-01 21:09:52 -0700

    removed old controller spec almost attempted

--- spec/controllers/error_handling_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe UsersController, type: :controller do
-  it "returns a not found response" do
-    get :show, { id: 'not_existing_page_321' }
-    expect(response.status).to eq(404)
-    expect(response.text).to match(/Page page_not_found doesn't exist/)
-end
-end
