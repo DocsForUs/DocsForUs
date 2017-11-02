f396088efa1f6652918e151aa2dc579a23382b6a
Amanuel <yihdego@msn.com>
2017-11-02 13:59:24 -0700

    refactor with before_action method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include StatesHelper
   include FormVariablesHelper
-  
+
   def current_user
-    if session[:user_id]
-      current_user = User.find(session[:user_id])
-    else
-      current_user = nil
-    end
-    return current_user
+    current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authorize
+      flash.now[:alert] = "You must login or register to recommend a doctor"
+      redirect_to login_path unless current_user
   end
 end
