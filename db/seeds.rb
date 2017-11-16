969133fb8225c860a6eacbf01e0c97ad935bd59d
Piper Denney <denney.piper@gmail.com>
2017-11-15 17:04:22 -0800

    Remove insurance data module. Move insurance list to seeds and modify to remove uid.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
-include InsuranceDataHelper
 
-include InsuranceDataHelper
 
 first_name = ["Elizabeth", "Karen", "Michael", "Elesa", "Kevin"]
 
@@ -20,12 +18,14 @@ Doctor.create(first_name: 'Eliza', last_name: "Brine", street: "1 Main Street", 
 Doctor.create(first_name: 'Eliza', last_name: "Brine", street: "1 Main Street", specialty: specialties.sample, city: 'New York', state: "NY", email_address:"dr@doctor.com", zipcode: "09099")
 Doctor.create(first_name: 'Eliza', last_name: "Brine", street: "1 Main Street", specialty: specialties.sample, city: 'Syracuse', state: "NY", email_address:"dr@doctor.com", zipcode: "09099")
 
-insurances = InsuranceDataHelper.get_insurance
-insurances.length.times do |i|
-  ins = Insurance.create(insurance_uid: insurances[i][1], insurance_name: insurances[i][0])
+
+
+insurance_list = ["Advantage Health Solutions", "Aetna", "Altius Coventry Health Care", "Ambetter Superior Health Plan", "Amerihealth", "Ameritas", "Anthem", "Assurant", "Aultcare", "BCBS Blue Card", "BCBS Minnesota", "Blue Cross Blue Sheild of Alabama", "Blue Cross Blue Shield of Aritona", "Blue Cross Blue Shield of Florida", "Blue Cross Blue Shield of Florida BCBSFL", "Blue Cross Blue Shield of Georgia", "Blue Cross Blue Shield of Illinois", "Blue Cross Blue Shield of Kansas City", "Blue Cross Blue Shield of Louisiana", "Blue Cross Blue Shield of Massachusetts", "Blue Cross Blue Shield of Michigan", "Blue Cross Blue Shield of New Mexico", "Blue Cross Blue Shield of Oklahoma", "Blue Cross Blue Shield of Rhode Island", "Blue Cross Blue Shield of TX", "Blue Cross Blue Shield of Tennessee", "Blue Cross Blue Shield of Texas", "Blue Cross Blue Shield of Texas BCBSTX", "Blue Cross Blue Shield of Wyoming", "Blue Cross Idaho", "Blue Shield of California", "Bluegrass Family Health", "Bridgespan", "Buckeye Community Health Plan", "Capital Blue Cross", "Capital District Physicians Health Plan", "Care First Blue Cross Blue Shield", "Chinese Community Health Plan", "Cigna", "Cigna Dental", "Cofinity", "CommUnityCare Health Centers", "CompBenefits Corporation", "Connecticare", "Coventry Health Care", "Delta Dental", "Dental Network of America", "DenteMax", "Diversified Dental", "Dominion Group", "Emblem Health", "Empire BCBS", "Excellus BCBS", "First Choice", "First Dental Health", "First Health Coventry Healthcare", "GWH-Cigna", "Geisinger Health Plan", "Group Health Cooperative", "Guardian Dental", "Gundersen Health Plan", "HAP", "HIP of NY", "HMSA", "Harvard Pilgrim", "Health Alliance", "Health Net", "Health Plan of Nevada", "HealthFirst NY", "HealthLink", "HealthNow", "HealthPlus of Michigan", "HealthSpan", "Highmark Blue Cross Blue Shield", "Home State Health Plan", "Horizon BCBS", "Humana", "IBC  Amerihealth", "Independence Blue Cross", "Independent Health Association", "Kaiser - MidAtlantic", "Kaiser Permanente", "L.A. Care Health", "Liberty Dental Plan", "Lifewise health Plan of Oregon", "MHS Indiana", "MHealth Insured", "MVP Health Plan", "MagnaCare", "Medicaid", "Medical Mutual of Ohio", "Medicare", "MetLife", "Molina Healthcare", "Multiplan", "ODS Health Plan", "Oxford Health", "PacificSource Health Plans", "Paramount", "Premera Blue Cross", "Premera Blue Cross Blue Shield", "Presbyterian Healthcare Services", "Principal Financial Services", "PriorityHealth", "Providence Health System", "QualCare", "Qualchoice", "Regence Blue Cross Blue Shield of Oregon", "Regence Blue Cross Blue Shield of Utah", "Regence Blue Shield of Idaho", "Regence Blue Shield of Washington", "Rocky Mountain Health Plans", "Sagamore Health Network", "Scion Dental", "Scott and White", "Sharp Health Plan", "Sierra Health", "Spectrum Health", "Total Dental Administrators", "Total Health Plan", "Tufts Health Plan", "UHC West Formerly Pacificare", "UHC of the River Valley", "United Concordia", "United Healthcare", "United Healthcare Dental", "Unity Health Insurance", "VSP", "Wellmark Blue Cross and Blue Shield", "Western Health Advantage", "Wisconsin Physicians Service Insurance Corporation"]
+
+insurance_list.each do |insurance|
+  ins = Insurance.create(insurance_name: insurance)
   Doctor.all.sample.insurances << ins
 end
-
 
 User.create(username:'admin', email: ENV['admin_email'], password: ENV['admin_password'], admin: true, superadmin: true)
 Tag.destroy_all
